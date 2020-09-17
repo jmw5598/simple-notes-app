9510ffe102f2ef0073b99067d3a06bf57614d0fe
Jason White <jmw5598@gmail.com>
2020-09-17 14:52:13 -0400

    Updated store with topics delete effect and reducer

--- client/src/app/core/store/effects/topic.effects.ts
+++ client/src/app/core/store/effects/topic.effects.ts
@@ -3,7 +3,7 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 
 import { TopicsService } from '../../services/topics.service';
 import { handleHttpError } from '../actions/http-error.actions';
-import { TopicActions, getAllTopicsSuccess, createTopicSuccess } from '../actions/topic.actions';
+import { TopicActions, getAllTopicsSuccess, createTopicSuccess, deleteTopicSuccess } from '../actions/topic.actions';
 
 import { of } from 'rxjs';
 import { map, mergeMap, catchError } from 'rxjs/operators';
@@ -30,6 +30,16 @@ export class TopicEffects {
     mergeMap(({topic}) => this._topicsService.save(topic)
       .pipe(
         map(result => createTopicSuccess({ topic: result })),
+        catchError(error => of(handleHttpError(error)))
+      )
+    )
+  ));
+
+  deleteTopic$ = createEffect(() => this._actions.pipe(
+    ofType(TopicActions.DELETE_TOPIC),
+    mergeMap(({id}) => this._topicsService.delete(id)
+      .pipe(
+        map(result => deleteTopicSuccess({ topic: result })),
         catchError(error => of(handleHttpError(error)))
       )
     )
--- client/src/app/core/store/reducers/topic.reducer.ts
+++ client/src/app/core/store/reducers/topic.reducer.ts
@@ -1,10 +1,19 @@
 import { createReducer, on } from '@ngrx/store';
-import { getAllTopicsSuccess } from '../actions/topic.actions';
+import { getAllTopicsSuccess, deleteTopicSuccess } from '../actions/topic.actions';
 import { initialTopicState } from '../state/topic.state';
+import { Topic } from '@sn/shared/models';
 
 const _topicReducer = createReducer(
   initialTopicState,
   on(getAllTopicsSuccess, (state, { topics }) => {
+    return {
+      ...state,
+      topics: topics
+    }
+  }),
+  on(deleteTopicSuccess, (state, { topic }) => {
+    console.log("parse out topic by id and update topics")
+    const topics: Topic[] = state.topics.filter(t => t.id === topic.id);
     return {
       ...state,
       topics: topics
