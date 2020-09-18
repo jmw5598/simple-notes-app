f6d98fa8570cf3506f64bb1c683ed4b32f646f7f
Jason White <jmw5598@gmail.com>
2020-09-18 08:55:14 -0400

    Updated topic details component with delete functionality, and small template changes

--- client/src/app/dashboard/topics/pages/topic-details/topic-details.component.ts
+++ client/src/app/dashboard/topics/pages/topic-details/topic-details.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 import { IAppState } from '@sn/core/store/state';
 import { selectSelectedTopic } from '@sn/core/store/selectors';
+import { setSelectedTopic, deleteSection } from '@sn/core/store/actions';
 import { Topic } from '@sn/shared/models';
 import { fadeAnimation } from '@sn/shared/animations';
 
@@ -12,14 +14,29 @@ import { fadeAnimation } from '@sn/shared/animations';
   styleUrls: ['./topic-details.component.scss'],
   animations: [fadeAnimation]
 })
-export class TopicDetailsComponent implements OnInit {
+export class TopicDetailsComponent implements OnInit, OnDestroy {
   public topic$: Observable<Topic>;
+  private _topicId: number;
 
   constructor(
     private _store: Store<IAppState>
   ) { }
 
   ngOnInit(): void {
-    this.topic$ = this._store.select(selectSelectedTopic);
+    this.topic$ = this._store.select(selectSelectedTopic)
+      .pipe(tap((topic: Topic) => {
+        this._topicId = topic.id;
+      }));
+  }
+
+  public delete(sectionId: number): void {
+    this._store.dispatch(deleteSection({
+      sectionId: sectionId,
+      topicId: this._topicId
+    }))
+  }
+
+  ngOnDestroy(): void {
+    this._store.dispatch(setSelectedTopic(null));
   }
 }
