b05a67f1800436502f2a14bf1d2cace1413891ee
Jason White <jmw5598@gmail.com>
2020-09-19 19:13:17 -0400

    Updated topics service to export topic to file by topic Id

--- client/src/app/core/services/topics.service.ts
+++ client/src/app/core/services/topics.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AbstractCrudService } from './abstract-crud.service';
-import { Topic, ExportConfig } from '@sn/shared/models';
+import { Topic, ExportConfig, FileResponse } from '@sn/shared/models';
 import { environment } from '@env/environment'
 
 @Injectable({
@@ -14,11 +14,14 @@ export class TopicsService extends AbstractCrudService<Topic, number> {
     super(_http, `${environment.api.baseUrl}/topics`);
   }
 
-  public exportTopic(topicId: number, config: ExportConfig): Observable<Blob> {
-    // TODO - May have to pipe and transforms the respose to Blob??
-    return this._http.post<Blob>(`${environment.api.baseUrl}/topics/${topicId}/download`, config)
-      .pipe(map(res => {
-        return new Blob();
-      }));
+  public exportTopic(topicId: number, config: ExportConfig): Observable<FileResponse> {
+    return this._http.post(`${environment.api.baseUrl}/topics/${topicId}/download`, config, { observe: 'response', responseType: 'blob', })
+      .pipe(map(response => this._extractFile(response)));
+  }
+
+  private _extractFile(res: Response | any) {
+    const header = res.headers.get('Content-Disposition');
+    const filename = header.substring(header.indexOf('filename'), header.length).split("=")[1];
+    return new FileResponse(res.body, filename);
   }
 }
