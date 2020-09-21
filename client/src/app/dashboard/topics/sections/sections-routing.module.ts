f033d198affdcd0b369ea3bed9a87654465d3d1f
Jason White <jmw5598@gmail.com>
2020-09-21 18:20:11 -0400

    Updated router path for section editor to editor instead of edit

--- client/src/app/dashboard/topics/sections/sections-routing.module.ts
+++ client/src/app/dashboard/topics/sections/sections-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     canActivate: [SectionByIdGuard],
     children: [
       {
-        path: 'edit',
+        path: 'editor',
         component: EditSectionComponent
       }
     ]
