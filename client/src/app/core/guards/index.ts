51ce64da7f08ee908912b30dd8d2c67157d79e53
Jason White <jmw5598@gmail.com>
2020-09-23 15:34:27 -0400

    Created account guards to populate state with account details

--- client/src/app/core/guards/index.ts
+++ client/src/app/core/guards/index.ts
@@ -3,3 +3,5 @@ export * from './plans.guard';
 export * from './topics.guard';
 export * from './topic-by-id.guard';
 export * from './section-by-id.guard';
+export * from './account-details.guard';
+export * from './account-profile.guard';
