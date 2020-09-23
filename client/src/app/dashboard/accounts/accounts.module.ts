2103796c2a07b7582770cc55d5d69bb74c6535e4
Jason White <jmw5598@gmail.com>
2020-09-23 15:35:20 -0400

    Created a rough accounts module to display user account details.  Allows of updates on profile section

---
+++ client/src/app/dashboard/accounts/accounts.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '@sn/shared/shared.module';
+import { AccountsRoutingModule } from './accounts-routing.module';
+import { AccountDetailsComponent } from './pages/account-details/account-details.component';
+import { AccountDetailsDisplayComponent } from './components/account-details-display/account-details-display.component';
+import { AccountDetailsFormComponent } from './components/account-details-form/account-details-form.component';
+
+@NgModule({
+  declarations: [
+    AccountDetailsComponent,
+    AccountDetailsDisplayComponent,
+    AccountDetailsFormComponent
+  ],
+  imports: [
+    CommonModule,
+    AccountsRoutingModule,
+    SharedModule 
+  ]
+})
+export class AccountsModule { }
--- client/src/app/dashboard/dashboard-routing.module.ts
+++ client/src/app/dashboard/dashboard-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountsRoutingModule } from './accounts/accounts-routing.module';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
@@ -7,6 +8,10 @@ const routes: Routes = [
     path: '',
     component: DashboardComponent,
     children: [
+      {
+        path: 'accounts',
+        loadChildren: () => import('./accounts/accounts.module').then(m => m.AccountsModule)
+      },
       {
         path: 'topics',
         loadChildren: () => import('./topics/topics.module').then(m => m.TopicsModule)
