508d024880d5c8a9d2a6e25f1a6f6eeb01054ab9
Jason White <jmw5598@gmail.com>
2020-09-24 09:01:45 -0400

    Created topic service method to search topics

--- client/src/app/core/services/topics.service.ts
+++ client/src/app/core/services/topics.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AbstractCrudService } from './abstract-crud.service';
 import { Topic, ExportConfig, FileResponse } from '@sn/shared/models';
+import { Page, IPageable } from '@sn/core/models';
 import { environment } from '@env/environment'
 
 @Injectable({
@@ -17,6 +18,17 @@ export class TopicsService extends AbstractCrudService<Topic, number> {
   public exportTopic(topicId: number, config: ExportConfig): Observable<FileResponse> {
     return this._http.post(`${environment.api.baseUrl}/topics/${topicId}/download`, config, { observe: 'response', responseType: 'blob', })
       .pipe(map(response => this._extractFile(response)));
+  }
+
+  public searchTopics(searchTerm: string, page?: IPageable): Observable<Page<Topic>> {
+    const params: {[key: string]: any} = !page ? {} : {
+      searchTerm: searchTerm,
+      page: page.page,
+      size: page.size,
+      sortCol: page.sort.column,
+      sortDir: page.sort.direction
+    };
+    return this._http.get<Page<Topic>>(`${this._base}/search`, { params: params });
   }
 
   private _extractFile(res: Response | any) {
