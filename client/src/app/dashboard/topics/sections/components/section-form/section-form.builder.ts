3f3d18dc4f252673b748bdc671d9e2cf8f2acc9f
Jason White <jmw5598@gmail.com>
2020-10-11 12:34:24 -0400

    topics modules was moved to module folder, deleting old module

--- client/src/app/dashboard/topics/sections/components/section-form/section-form.builder.ts
+++
@@ -1,9 +0,0 @@
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-
-export const buildSectionFormGroup = (formBuilder: FormBuilder): FormGroup => {
-  return formBuilder.group({
-    id: [''],
-    title: ['', Validators.required],
-    synopsis: ['', Validators.required]
-  });
-}
