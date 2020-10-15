7a80aa9abc5ef8c2f091dc926a85b7c677c1bf9a
Jason White <jmw5598@gmail.com>
2020-10-15 10:47:13 -0400

    Update section form to auto focus on title when rendered

--- client/src/app/dashboard/modules/topics/components/section-form/section-form.component.ts
+++ client/src/app/dashboard/modules/topics/components/section-form/section-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Renderer2 } from '@angular/core';
 import { ControlContainer, FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,12 +6,25 @@ import { ControlContainer, FormGroup } from '@angular/forms';
   templateUrl: './section-form.component.html',
   styleUrls: ['./section-form.component.scss']
 })
-export class SectionFormComponent implements OnInit {
+export class SectionFormComponent implements OnInit, AfterViewInit {
   public form: FormGroup;
 
-  constructor(private _parentControl: ControlContainer) { }
+  constructor(
+    private _renderer: Renderer2,
+    private _parentControl: ControlContainer
+  ) { }
 
   ngOnInit(): void {
     this.form = this._parentControl.control as FormGroup;
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this._setFocusToTitleInput();
+    });
+  }
+
+  private _setFocusToTitleInput(): void {
+    this._renderer.selectRootElement('#title').focus();
   }
 }
