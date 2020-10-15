1f10e14188ee0a153656a83169934c3cf19ec1a8
Jason White <jmw5598@gmail.com>
2020-10-15 13:12:19 -0400

    Started building topic search component for the side panel drawer component

--- client/src/app/shared/components/topic-search/topic-search.component.ts
+++ client/src/app/shared/components/topic-search/topic-search.component.ts
@@ -1,25 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { IAppState } from '@sn/core/store/state';
+import { fadeAnimation } from '@sn/shared/animations';
+import { selectSearchTopicsResult } from '@sn/core/store/selectors';
+import { Page } from '@sn/core/models';
+import { Topic } from '@sn/shared/models';
+import { IPageable } from '@sn/core/models';
+import { PageableSearch } from '@sn/core/models'
+import { DEFAULT_SEARCH_TOPICS_PAGE } from '@sn/core/defaults';
+import { searchTopics, searchTopicsResult } from '@sn/core/store/actions';
+import { DrawerService } from '@sn/shared/components';
 
 @Component({
   selector: 'sn-topic-search',
   templateUrl: './topic-search.component.html',
-  styleUrls: ['./topic-search.component.scss']
+  styleUrls: ['./topic-search.component.scss'],
+  animations: [fadeAnimation]
 })
-export class TopicSearchComponent implements OnInit {
-  public form: FormGroup;
+export class TopicSearchComponent implements OnInit, OnDestroy {
+  private readonly DEFAULT_PAGE: IPageable = DEFAULT_SEARCH_TOPICS_PAGE;
+  public searchResults$: Observable<Page<Topic>>;
 
   constructor(
-    private _formBuilder: FormBuilder
+    private _router: Router,
+    private _store: Store<IAppState>,
+    private _drawerService: DrawerService
   ) { }
 
   ngOnInit(): void {
-    this.form = this._formBuilder.group({
-      searchTerms: ['', [Validators.required]]
-    })
+    this.searchResults$ = this._store.select(selectSearchTopicsResult);
   }
 
-  public onSearch(value: any): void {
-    console.log('Search.....');
+  // TODO Create separate piece of state for side panel search vs list search
+  // This is because the list search is filtered by the same piece of state so
+  // when the drawer is close and you navigate to topic list view, the list is
+  // filtered base on the drawer search.
+  // Tried clearing on onDestory of the drawer search compoent but wasnt working.
+
+  public onSearchTopics(value: any): void {
+    if (value) {
+      const topicSearch: PageableSearch = {
+        searchTerm: value,
+        pageable: this.DEFAULT_PAGE
+      };
+      this._store.dispatch(searchTopics({ search: topicSearch }));
+    } else {
+      this._store.dispatch(searchTopicsResult({ page: null }));
+    }
+  }
+
+  public onGoToTopic(id: number): void {
+    this._router.navigate(['/dashboard', 'topics', id, 'details']);
+    this._drawerService.close();
   }
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { TagInputComponent } from './components/tag-input/tag-input.component';
@@ -39,7 +40,8 @@ import { DebounceDirective } from './directives/debounce.directive';
     FormsModule,
     ReactiveFormsModule,
     TimepickerModule,
-    BsDatepickerModule.forRoot()
+    BsDatepickerModule.forRoot(),
+    RouterModule
   ],
   exports: [
     FormsModule,
