0043681b7244b4505f2c7d7fd84b7953e88b9a5d
Jason White <jmw5598@gmail.com>
2020-10-22 11:17:18 -0400

    Updated color picker, disabled manual hex input, updated available colors

--- client/src/app/shared/defaults/colors.defaults.ts
+++ client/src/app/shared/defaults/colors.defaults.ts
@@ -1,12 +1,12 @@
 export const HEX_COLOR_STRING_ARRAY: string[] = [
   '#158cba', // Blue
-  '#6610f2', // Indigo
   '#6f42c1', // Purple
   '#e83e8c', // Pink
   '#ff4136', // Red
   '#fd7e14', // Orange
+  '#f1e35c', // Yellow
   '#28b62c', // Green
-  // '#', // 
+  '#75caeb', // Cyan
   // '#', // 
   // '#', // 
   // '#', // 
