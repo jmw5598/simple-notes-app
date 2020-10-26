1d19c53991bd4084b2dc32ec9339fdb10fd73417
Jason White <jmw5598@gmail.com>
2020-10-26 13:47:37 -0400

    WIP: Refactored account module, old functionality now working again after refactore.  Still have to create integrations for google calendar

--- client/src/app/modules/dashboard/modules/accounts/accounts.module.ts
+++ client/src/app/modules/dashboard/modules/accounts/accounts.module.ts
@@ -4,17 +4,11 @@ import { SharedModule } from '@sn/shared/shared.module';
 
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { AccountsRoutingModule } from './accounts-routing.module';
-import { AccountDetailsComponent } from './pages/account-details/account-details.component';
-import { AccountDetailsDisplayComponent } from './components/account-details-display/account-details-display.component';
-import { AccountDetailsFormComponent } from './components/account-details-form/account-details-form.component';
 import { AccountSettingsComponent } from './pages/account-settings/account-settings.component';
 import { AccountUserCardComponent } from './components/account-user-card/account-user-card.component';
 
 @NgModule({
   declarations: [
-    AccountDetailsComponent,
-    AccountDetailsDisplayComponent,
-    AccountDetailsFormComponent,
     AccountSettingsComponent,
     AccountUserCardComponent
   ],
--- client/src/app/modules/dashboard/modules/accounts/modules/account-settings-general/pages/account-settings-general/account-settings-general.component.ts
+++ client/src/app/modules/dashboard/modules/accounts/modules/account-settings-general/pages/account-settings-general/account-settings-general.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Account, Profile } from '@sn/core/models';
+import { Store } from '@ngrx/store';
+import { IAppState } from '@sn/core/store/state';
+import { selectAccountDetails, selectAccountProfile } from '@sn/core/store/selectors';
 import { fadeAnimation } from '@sn/shared/animations';
+import { AccountValidators } from '@sn/core/validators';
+import { buildProfileFormGroup } from '@sn/shared/forms';
+import { updateAccountProfile } from '@sn/core/store/actions';
 
 @Component({
   selector: 'sn-account-settings-general',
@@ -8,10 +18,47 @@ import { fadeAnimation } from '@sn/shared/animations';
   animations: [fadeAnimation]
 })
 export class AccountSettingsGeneralComponent implements OnInit {
+  public form: FormGroup;
+  public isEditingProfile: boolean;
+  public accountDetails$: Observable<Account>;
+  public accountProfile$: Observable<Profile>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _store: Store<IAppState>,
+    private _formBuilder: FormBuilder,
+    private _accountValidators: AccountValidators
+  ) {
+    this.isEditingProfile = false;
   }
 
+  ngOnInit(): void {
+    this.accountDetails$ = this._store.select(selectAccountDetails);
+    this.accountProfile$ = this._store.select(selectAccountProfile).pipe(
+      tap((profile: Profile) => {
+        if (profile) {
+          this.form.get('profile').patchValue(profile);
+        }
+      })
+    );
+    this.form = this._formBuilder.group({
+      profile: buildProfileFormGroup(this._formBuilder, this._accountValidators)
+    });
+  }
+
+  public editProfile(isEditingProfile: boolean): void {
+    this.isEditingProfile = isEditingProfile;
+  }
+
+  public onEditingProfile(isEditingProfile: boolean): void {
+    this.isEditingProfile = isEditingProfile;
+  }
+
+  public onUpdateProfile(formValue: { profile: Profile }): void {
+    this._store.dispatch(updateAccountProfile({ profile: formValue.profile }));
+    this.onEditingProfile(false);
+  }
+
+  public updateProfile(formValue: { profile: Profile }): void {
+    this.isEditingProfile = false;
+  }
 }
