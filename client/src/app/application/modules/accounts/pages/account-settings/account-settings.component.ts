198cff4f4236a9815b43d44be0b38c3058cb95b5
Jason White <jmw5598@gmail.com>
2020-11-01 08:15:23 -0500

    Fixed issue with account setting tabs.  Now proper active tab is set base on path

--- client/src/app/application/modules/accounts/pages/account-settings/account-settings.component.ts
+++ client/src/app/application/modules/accounts/pages/account-settings/account-settings.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Router, ActivatedRoute, UrlSegment } from '@angular/router';
 import { fadeAnimation } from '@sn/shared/animations'
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { IAccountsState } from '../../store/reducers';
 import { Account, Profile } from '@sn/core/models';
 import { selectAccountDetails, selectAccountProfile } from '../../store/selectors';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'sn-account-settings',
@@ -13,18 +14,32 @@ import { selectAccountDetails, selectAccountProfile } from '../../store/selector
   styleUrls: ['./account-settings.component.scss'],
   animations: [fadeAnimation]
 })
-export class AccountSettingsComponent implements OnInit {
+export class AccountSettingsComponent implements OnInit, AfterViewInit {
+  @ViewChild('tabs', { static: false })
+  public tabs: TabsetComponent;
+
   public accountProfile$: Observable<Profile>;
   public accountDetails$: Observable<Account>;
 
   constructor(
     private _router: Router,
+    private _route: ActivatedRoute,
     private _store: Store<IAccountsState>
   ) { }
 
   ngOnInit(): void {
     this.accountProfile$ = this._store.select(selectAccountProfile);
     this.accountDetails$ = this._store.select(selectAccountDetails);
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      const currentTab: UrlSegment = this._route.firstChild.snapshot.url[0];
+      const tab = this.tabs.tabs.find(e => e.id === currentTab.path.toLowerCase());
+      if (tab) {
+        tab.active = true;
+      }
+    });
   }
 
   public navigateTo(route: string): void {
