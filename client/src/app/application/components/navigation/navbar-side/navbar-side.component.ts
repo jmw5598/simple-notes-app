95c7ac28ba413a869b0090143ad635ec12fc19f9
Jason White <jmw5598@gmail.com>
2020-12-12 09:37:57 -0500

    Fix nav responsiveness for mobile

--- client/src/app/application/components/navigation/navbar-side/navbar-side.component.ts
+++ client/src/app/application/components/navigation/navbar-side/navbar-side.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LayoutService, LayoutSidePanelState } from '@sn/shared/components'
 import { NavigationRouteLink, NAVIGATION_ROUTES } from './navigation.routes';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'sn-navbar-side',
@@ -10,12 +11,36 @@ import { NavigationRouteLink, NAVIGATION_ROUTES } from './navigation.routes';
 })
 export class NavbarSideComponent implements OnInit {
   public LayoutSidePanelState = LayoutSidePanelState
-  public currentPanelState$: Observable<LayoutSidePanelState>;
+  
   public links: NavigationRouteLink[] = NAVIGATION_ROUTES;
+
+  private _currentScreenWidth: number;
+  public currentPanelState$: Observable<LayoutSidePanelState>;
+  public currentLayoutSidePanelState: LayoutSidePanelState;
 
   constructor(private _layoutService: LayoutService) { }
 
   ngOnInit() {
-    this.currentPanelState$ = this._layoutService.onStateChanges();
+    this.currentPanelState$ = this._layoutService.onStateChanges()
+      .pipe(tap(state => this.currentLayoutSidePanelState = state));
+  }
+
+  public close(): void {
+    if (this.currentLayoutSidePanelState === LayoutSidePanelState.OPEN) {
+      this._shouldBeClosed() 
+        ? this._layoutService.changeState(LayoutSidePanelState.CLOSE)
+        : this._layoutService.changeState(LayoutSidePanelState.COLLAPSE)
+    } else {
+      this._layoutService.changeState(LayoutSidePanelState.OPEN);
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this._currentScreenWidth = window.innerWidth;
+  }
+
+  private _shouldBeClosed(): boolean {
+    return this._currentScreenWidth !== undefined && this._currentScreenWidth < 768;
   }
 }
