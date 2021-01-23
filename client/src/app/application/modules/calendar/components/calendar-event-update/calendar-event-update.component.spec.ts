bca0f43ddb9313ae09e2bf4e260e7d3bffd6d60a
Jason White <jmw5598@gmail.com>
2021-01-23 15:58:49 -0500

    Created unit tests for some calendar components

--- client/src/app/application/modules/calendar/components/calendar-event-update/calendar-event-update.component.spec.ts
+++ client/src/app/application/modules/calendar/components/calendar-event-update/calendar-event-update.component.spec.ts
@@ -1,8 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ControlContainer, FormControl, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CalendarEvent } from '@sn/core/models';
 import { CalendarEventUpdateComponent } from './calendar-event-update.component';
 
-describe('CalendarEventUpdateComponent', () => {
+fdescribe('CalendarEventUpdateComponent', () => {
   let component: CalendarEventUpdateComponent;
   let fixture: ComponentFixture<CalendarEventUpdateComponent>;
 
@@ -23,6 +24,20 @@ describe('CalendarEventUpdateComponent', () => {
     description: new FormControl('', [Validators.required]),
   });
   testFormGroupDirective.form = testFormGroup;
+
+  const mockCalendarEvent: CalendarEvent = {
+    id: 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+    startDateTime: new Date(),
+    endDateTime: new Date(),
+    description: 'Mock Description',
+    title: 'Mock Title',
+    isAllDay: false,
+    location: 'Mock Location',
+    color: '#000000'
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -47,9 +62,23 @@ describe('CalendarEventUpdateComponent', () => {
     fixture = TestBed.createComponent(CalendarEventUpdateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    jasmine.clock().install();
+  });
+
+  afterEach(() => {
+    jasmine.clock().uninstall();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should patch mock calendar event to form and form should be valid', () => {
+    component.event = mockCalendarEvent;
+    component.ngOnInit();
+    component.ngAfterViewInit();
+    fixture.detectChanges();
+    jasmine.clock().tick(1);
+    expect(component.form.valid).toBeTrue();
   });
 });
