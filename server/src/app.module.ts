58121302a38749b18e63e5fcee4843d1f9155615
Jason White <jmw5598@gmail.com>
2021-05-16 06:33:48 -0400

    Got flashcard flipping now

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -30,13 +30,7 @@ import { FlashcardsModule } from './flashcards/flashcards.module';
     CalendarModule,
     ThemesModule,
     DocumentBuilderModule,
-    CacheModule.register({
-      store: redisStore,
-      host: `${process.env.REDIS_HOST}`,
-      port: `${process.env.REDIS_PORT}`,
-      user: `${process.env.REDIS_USER}`,
-      password: `${process.env.REDIS_PASSWORD}`
-    }),
+    CacheModule.register(),
     FlashcardsModule
   ],
   controllers: [],
