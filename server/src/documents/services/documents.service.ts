66fa9fb256f2335c3b071631e8f3b21e520e07eb
Jason White <jmw5598@gmail.com>
2021-06-06 07:44:59 -0400

    Fixed issue with fetching documents with topic but no sections

--- server/src/documents/services/documents.service.ts
+++ server/src/documents/services/documents.service.ts
@@ -15,6 +15,7 @@ import { DocumentMarkdownDto } from '../dtos/document-markdown.dto';
 import { DocumentBuilderService } from '../../common/services/document-builder/services/document-builder.service';
 import { ExportConfig } from 'src/common/services/document-builder/models/export-config.model';
 import { Readable } from 'typeorm/platform/PlatformTools';
+import { doc } from 'prettier';
 
 @Injectable()
 export class DocumentsService {
@@ -77,43 +78,15 @@ export class DocumentsService {
   }
 
   public async getDocumentById(accountId: number, documentId: number): Promise<DocumentDto> {
-    // TODO Check if sections are returned with notes, we need to exclude notes for this...
-    const document: Document = await this._documentsRepository.createQueryBuilder('doc')
-      .innerJoin('doc.account', 'account')
-      .innerJoinAndSelect('doc.documentTopics', 'dt')
-      .innerJoinAndSelect('dt.topic', 'topic')
-      .innerJoinAndSelect('dt.documentTopicSections', 'dts')
-      .innerJoinAndSelect('dts.section', 's')
-      .where('account.id = :accountId', { accountId: accountId })
-      .andWhere('doc.id = :documentId', { documentId: documentId })
-      .orderBy({ 
-        'dt.orderIndex': 'ASC',
-        'dts.orderIndex': 'ASC'
-      }).getOne();
-
+    const document: Document = await this._findDocumentById(accountId, documentId);
     if (!document) throw new DocumentNotFoundException();
-
     return DocumentMapper.toDocumentDto(document);
   }
 
   public async getDocumentMarkdownById(accountId: number, documentId: number): Promise<DocumentMarkdownDto> {
-    const document: Document = await this._documentsRepository.createQueryBuilder('doc')
-      .innerJoin('doc.account', 'account')
-      .innerJoinAndSelect('doc.documentTopics', 'dt')
-      .innerJoinAndSelect('dt.topic', 'topic')
-      .innerJoinAndSelect('dt.documentTopicSections', 'dts')
-      .innerJoinAndSelect('dts.section', 's')
-      .where('account.id = :accountId', { accountId: accountId })
-      .andWhere('doc.id = :documentId', { documentId: documentId })
-      .orderBy({ 
-        'dt.orderIndex': 'ASC',
-        'dts.orderIndex': 'ASC'
-      }).getOne();
-
+    const document: Document = await this._findDocumentById(accountId, documentId);
     if (!document) throw new DocumentNotFoundException();
-
     const markdownTopicString: string = this._documentBuilderService.documentToMarkdown(document);
-
     return {
       document: await this.getDocumentById(accountId, documentId),
       markdown: markdownTopicString || '' 
@@ -179,21 +152,8 @@ export class DocumentsService {
   }
 
   public async exportDocument(accountId: number, documentId: number, exportConfig: ExportConfig): Promise<Readable> {
-    const document: Document = await this._documentsRepository.createQueryBuilder('doc')
-      .innerJoin('doc.account', 'account')
-      .innerJoinAndSelect('doc.documentTopics', 'dt')
-      .innerJoinAndSelect('dt.topic', 'topic')
-      .innerJoinAndSelect('dt.documentTopicSections', 'dts')
-      .innerJoinAndSelect('dts.section', 's')
-      .where('account.id = :accountId', { accountId: accountId })
-      .andWhere('doc.id = :documentId', { documentId: documentId })
-      .orderBy({ 
-        'dt.orderIndex': 'ASC',
-        'dts.orderIndex': 'ASC'
-      }).getOne();
-
+    const document: Document = await this._findDocumentById(accountId, documentId);
     if (!document) throw new DocumentNotFoundException();
-
     return this._documentBuilderService.exportDocument(document, exportConfig);
   }
 
@@ -213,5 +173,20 @@ export class DocumentsService {
       next: pageable.next(totalElements),
       previous: pageable.previous(totalElements)
     } as Page<DocumentDto>;
+  }
+
+  private async _findDocumentById(accountId: number, documentId: number): Promise<Document> {
+    return this._documentsRepository.createQueryBuilder('doc')
+      .innerJoin('doc.account', 'account')
+      .innerJoinAndSelect('doc.documentTopics', 'dt')
+      .innerJoinAndSelect('dt.topic', 'topic')
+      .leftJoinAndSelect('dt.documentTopicSections', 'dts')
+      .leftJoinAndSelect('dts.section', 's')
+      .where('account.id = :accountId', { accountId: accountId })
+      .andWhere('doc.id = :documentId', { documentId: documentId })
+      .orderBy({ 
+        'dt.orderIndex': 'ASC',
+        'dts.orderIndex': 'ASC'
+      }).getOne();
   }
 }
