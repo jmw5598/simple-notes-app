5c0f9204566112bc694fbc45442e41f17e1ecc79
Jason White <jmw5598@gmail.com>
2021-06-13 09:07:31 -0400

    Started mocking out dashboard for admin site

--- clients/projects/admin/src/app/application/modules/dashboard/pages/dashboard-overview/dashboard-overview.component.ts
+++ clients/projects/admin/src/app/application/modules/dashboard/pages/dashboard-overview/dashboard-overview.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { fadeAnimation } from '@sn/shared/animations';
+import { AbstractPageOverlayLoader, OverlayLoaderService } from '@sn/shared/components';
+
 @Component({
   selector: 'sn-admin-dashboard-overview',
   templateUrl: './dashboard-overview.component.html',
-  styleUrls: ['./dashboard-overview.component.scss']
+  styleUrls: ['./dashboard-overview.component.scss'],
+  animations: [fadeAnimation]
 })
-export class DashboardOverviewComponent implements OnInit {
+export class DashboardOverviewComponent extends AbstractPageOverlayLoader implements OnInit {
 
-  constructor() { }
+  constructor(
+    protected _overlayLoaderService: OverlayLoaderService
+  ) {
+    super(_overlayLoaderService);
+  }
 
   ngOnInit(): void {
   }
-
 }
