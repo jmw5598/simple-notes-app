285258af19ce2dcd99913924fc43b1d6fb288ccc
Jason White <jmw5598@gmail.com>
2023-06-15 05:41:13 -0400

    create scripts directory, moved the build libs script command from package.json to its own shell script as it was getting long and annoying, should be much more readable now.

---
+++ clients/scripts/build-libs.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "\nBuilding project libs.....\n"
+
+ng build core-framing
+ng build shared-models
+ng build core-services
+ng build shared-animations
+ng build button
+ng build alert
+ng build checkbox
+ng build drawer
+ng build empty
+ng build toolbar
+ng build toaster
+ng build overlay-content
+ng build paginator
+ng build debounce-search
+ng build card
+ng build loading-spinner
+ng build markdown
+ng build tag-input
+ng build radio
+ng build section-header
+ng build form
+
+echo "\nProject libs built successfully!\n"
